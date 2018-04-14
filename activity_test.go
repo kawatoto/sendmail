7877076faba3011ff442b02fffc286caaf2f60c9
Antonio Davila <kawatoto@github.com>
2018-04-13 20:47:17 -0500

    Sent email acrivity first commit

---
+++ activity_test.go
@@ -0,0 +1,66 @@
+package sendmail
+
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+
+	"github.com/TIBCOSoftware/flogo-contrib/action/flow/test"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/stretchr/testify/assert"
+)
+
+var activityMetadata *activity.Metadata
+
+func getActivityMetadata() *activity.Metadata {
+
+	if activityMetadata == nil {
+		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
+		if err != nil {
+			panic("No Json Metadata found for activity.json path")
+		}
+
+		activityMetadata = activity.NewMetadata(string(jsonMetadataBytes))
+	}
+
+	return activityMetadata
+}
+
+func TestCreate(t *testing.T) {
+
+	act := NewActivity(getActivityMetadata())
+
+	if act == nil {
+		t.Error("Activity Not Created")
+		t.Fail()
+		return
+	}
+}
+
+func TestEval(t *testing.T) {
+
+	defer func() {
+		if r := recover(); r != nil {
+			t.Failed()
+			t.Errorf("panic during execution: %v", r)
+		}
+	}()
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	//setup attrs
+	to := "adavilag@tibco.com"
+	tc.SetInput("to", to)
+	tc.SetInput("from", "adavilag0600@gmail.com")
+	tc.SetInput("password", "Parachutes1")
+	tc.SetInput("subject", "Test from Flogo")
+	tc.SetInput("message", "Hi! This is a message sent from Flogo")
+
+	act.Eval(tc)
+
+	//check result attr
+	result := tc.GetOutput("result")
+	fmt.Printf("%v", result)
+	assert.Equal(t, result, "The email has been sent to "+to)
+}
